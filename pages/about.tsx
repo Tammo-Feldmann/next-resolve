a9ef51d38fc0af7cf903e67f7cb994e620a8544f
Tammo Feldmann <31222040+Tammo-Feldmann@users.noreply.github.com>
2021-06-26 16:00:13 -0500

    Add json text

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const About = (): JSX.Element => {
+  return (
+    <div>
+      <Head>
+        <title>Index Resolution</title>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <div>About</div>
+    </div>
+  )
+}
+
+export default About
